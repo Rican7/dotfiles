db3b7ff4cf5838c90effbe06719d75e663bf8b22
Trevor Suarez <rican7@gmail.com>
2011-04-21 04:33:21 -0400

    Added new install files/instructions

---
+++ FIXSYMLINKS.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+#
+# Script to fix all of the symlinks that were synced, to the correct home values 
+#
+#
+
+# Loop through each file in the usr local bin
+for filename in /usr/local/bin/*
+do
+	# Get the symlink target
+	target=$(readlink /usr/local/bin/ssh-psu)
+
+	# Check if file exists or not, if not, correct the link
+	if [[ ! -f "$target" ]] && [[ ! -d "$target" ]] ; then
+		# Get the links home path first
+		baseName=$($target | egrep -o "/.sh/(.*?)")
+
+		# If the symbolic link's home directory doesn't match our HOME, change it
+		if [[ "$linkHome" != "$HOME" ]] ; then
+			ln -snf "$HOME$baseName" $filename
+		fi
+	fi
+done;
