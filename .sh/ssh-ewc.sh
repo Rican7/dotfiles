2e84e2e7a7381c711f55dd6166b0935db629f7d7
Trevor Suarez <rican7@gmail.com>
2012-06-21 22:40:50 -0400

    Removing old unused scripts

--- .sh/ssh-ewc.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-#
-# Script to connect to EWC's SSH. So I don't have to remember the port number
-#
-#
-ssh -p 2022 root@easywebcreations.com
--- .sh/ssh-ura.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-#
-# Script to connect to PSU's dev server, so I don't have to type the long domain in... or remember my password
-#
-#
-ssh tnsuarez@uranus.dev.plymouth.edu
