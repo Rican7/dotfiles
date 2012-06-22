8882cfa998f773ddc7e911ab7567a8ebae600c39
Trevor Suarez <rican7@gmail.com>
2012-06-22 00:43:34 -0400

    More cleanup and configs made

--- .sh/.sh/ssh-ewc.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-#
-# Script to connect to EWC's SSH. So I don't have to remember the port number
-#
-#
-ssh -p 2022 root@easywebcreations.com
--- .sh/.sh/ssh-ura.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-#
-# Script to connect to PSU's dev server, so I don't have to type the long domain in... or remember my password
-#
-#
-ssh tnsuarez@uranus.dev.plymouth.edu
